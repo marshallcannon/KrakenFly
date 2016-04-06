41d2b6309c1b395c430a13e63f20884b782b69d2
Marshall <marshcannon@gmail.com>
2016-04-06 09:52:35 -0700

    Added Kraken texture

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ var game = new Phaser.Game(600, 800, Phaser.AUTO, 'game', { preload: preload, cr
 
 function preload() {
 
+  game.load.image('kraken', 'assets/textures/pixelKraken.png');
+
 }
 
 function create() {
 
+  var kraken = game.add.sprite(200, 200, 'kraken');
+
 }
 
 function update() {
-  
+
 }
