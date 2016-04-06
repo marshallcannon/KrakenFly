accc8234a8d98d8ec0fbbdbccba8de1eb20ff971
Marshall <marshcannon@gmail.com>
2016-04-06 09:12:40 -0700

    Added base files

---
+++ main.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(600, 800, Phaser.AUTO, 'game', { preload: preload, create: create, update: update }, false, false);
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+  
+}
